12fdc14847a246c82e6142efc1a47ac2def99e3a
Helder Santana <helder.css@gmail.com>
2012-10-16 19:32:17 -0300

    run module without instance

--- test/run.js
+++ test/run.js
@@ -3,6 +3,9 @@ var DataURI = require("../lib/exec.js"),
     _       = require("lodash"),
     uri,
     cases   = {
+        "should have the correct datauri format without instance" : function() {
+            assert.strictEqual( DataURI('./test/case.txt') , "data:text/plain;base64,Zm9vIGJhcgo=");
+        },
         "should instance DataURI object" : function() {
             uri = new DataURI('./test/case.txt');
         },
@@ -20,21 +23,6 @@ var DataURI = require("../lib/exec.js"),
         },
         "should return a css with Class Name defined by user" : function() {
             assert.strictEqual( uri.getCss("foobar") , "\n.foobar {\n    background: url('data:text/plain;base64,Zm9vIGJhcgo=')\n}");
-        },
-        "should exist base64 param" : function() {
-            assert.strictEqual( DataURI('./test/case.txt').base64 , "Zm9vIGJhcgo=");
-        },
-        "should the right mimetype" : function() {
-            assert.strictEqual( DataURI('./test/case.txt').mimetype , "text/plain");
-        },
-        "should have the correct datauri format" : function() {
-            assert.strictEqual( DataURI('./test/case.txt').content , "data:text/plain;base64,Zm9vIGJhcgo=");
-        },
-        "should return a css with Class Name equal filename" : function() {
-            assert.strictEqual( DataURI('./test/case.txt').getCss() , "\n.case {\n    background: url('data:text/plain;base64,Zm9vIGJhcgo=')\n}");
-        },
-        "should return a css with Class Name defined by user" : function() {
-            assert.strictEqual( DataURI('./test/case.txt').getCss("foobar") , "\n.foobar {\n    background: url('data:text/plain;base64,Zm9vIGJhcgo=')\n}");
         }
     };
 
