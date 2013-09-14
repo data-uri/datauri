4e135720f3e534bef66e9916358f221344e4e42e
Helder Santana <ping@heldr.com>
2013-09-13 22:40:15 -0400

    fix jshint errors on the last pull request #1

--- test/run.js
+++ test/run.js
@@ -5,11 +5,11 @@ var DataURI = require("../lib/exec.js"),
     remoteUri,
     cases   = {
         "should have the correct datauri format without instance" : function() {
-            assert.strictEqual( DataURI('./test/case.txt') , "data:text/plain;base64,Zm9vIGJhcgo=");
+            assert.strictEqual(DataURI('./test/case.txt'), "data:text/plain;base64,Zm9vIGJhcgo=");
         },
         "should instance DataURI object" : function() {
             uri = new DataURI('./test/case.txt');
-            assert.strictEqual( (uri instanceof DataURI) , true);
+            assert.strictEqual( (uri instanceof DataURI), true);
         },
         "should exist base64 param" : function() {
             assert.strictEqual( uri.base64 , "Zm9vIGJhcgo=");
