64ada3a2ed29aa652e01e3daf21976e88d687d2a
Helder Santana <helder.css@gmail.com>
2015-10-14 07:39:42 -0400

    get image dimensions

--- test/spec/cli.spec.js
+++ test/spec/cli.spec.js
@@ -1,114 +1,228 @@
-describe('Data-uri Client', function () {
-    'use strict';
+import fs from 'fs';
+import { should as Should } from 'chai';
+import { exec as execute } from 'child_process';
+import * as cssExp from '../expected/css';
 
-    var fs        = require('fs'),
-        should    = require('chai').should(),
-        fixture   = 'test/fixture.gif',
-        child     = require('child_process'),
-        execute   = child.exec,
-        cli       = './bin/datauri ',
-        expected  = new RegExp('data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'),
-        dUri, cssContent;
+const should = Should();
+const fixture = 'test/fixture.gif';
+const expected = new RegExp(
+  'data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7'
+);
 
+let cli = './bin/datauri';
 
-    describe('generate a data-uri string', function () {
-        it('should give advice when a user do not type anything after datauri', function (done) {
-            var expected = new RegExp('Data-uri usage:');
+if (process.argv.join('').match('watch')) {
+  cli += ' --env=watch'
+}
 
-            execute(cli, function (err, stdout) {
-                should.not.exist(err);
-                stdout.should.not.be.empty;
-                stdout.should.match(expected);
+let dUri, cssContent;
 
-                done();
-            });
-        });
+describe('Data-uri Client', () => {
 
-        it('should run datauri through a simple file', function (done) {
-            execute(cli + fixture, function (err, stdout) {
-                should.not.exist(err);
-                stdout.should.not.be.empty;
-                stdout.should.match(expected);
+  describe('generate a data-uri string', () => {
 
-                done();
-            });
-        });
+    it('should give advice when a user do not type anything after datauri', done => {
+      let expected = new RegExp('Data-uri usage:');
+
+      execute(cli, (err, stdout) => {
+        should.not.exist(err);
+        stdout.should.not.be.empty;
+        stdout.should.match(expected);
+
+        done();
+      });
     });
 
-    describe('generate a data-uri string into a css file', function () {
+    it('should run datauri through a simple file', done => {
+      execute(`${cli} ${fixture}`, (err, stdout) => {
+        should.not.exist(err);
+        stdout.should.not.be.empty;
+        stdout.should.match(expected);
 
-        var createdFile  = 'test/duality.css',
-            createdClass = "\n.fixture {\n    background-image: url('data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAAALAAAAAABAAEAAAIBRAA7');\n}";
+        done();
+      });
+    });
+  });
 
-        describe('create a css file', function () {
+  describe('generate a data-uri string into a css file', () => {
 
-            afterEach(function (done) {
-                fs.unlink(createdFile, done);
-            });
+    let createdFile = 'test/duality.css';
 
-            it('should insert a css class with the target file name', function (done) {
-                execute(cli + fixture + ' --css=' + createdFile, function (err, stdout) {
-                    should.not.exist(err);
-                    stdout.should.not.be.empty;
-                    stdout.should.match(/created/);
-                    fs.existsSync(createdFile).should.be.true;
-                    fs.readFileSync(createdFile, 'utf-8').should.equal(createdClass);
-                    done();
-                });
-            });
+    describe('create a css file', () => {
 
-            it('should insert a css class with a specific name', function (done) {
-                var cssClass    = 'pipoca',
-                    fileContent = createdClass.replace('fixture', cssClass);
+      afterEach(done => fs.unlink(createdFile, done));
 
-                execute(cli + fixture + ' --css=' + createdFile + ' --class=' + cssClass, function (err, stdout) {
-                    should.not.exist(err);
-                    stdout.should.not.be.empty;
-                    stdout.should.match(/created/);
-                    fs.existsSync(createdFile).should.be.true;
-                    fs.readFileSync(createdFile, 'utf-8').should.equal(fileContent);
-                    done();
-                });
-            });
+      it('should insert a css class with the target file name',
+        done => {
+          execute(`${cli} ${fixture} --css=${createdFile}`, (err,
+            stdout) => {
+            should.not.exist(err);
+            stdout.should.not.be.empty;
+            stdout.should.match(/created/);
+            fs.existsSync(createdFile).should.be.true;
+            fs.readFileSync(createdFile, 'utf-8').should.equal(cssExp.simple);
+            done();
+          });
         });
 
-        describe('update a css file', function () {
+      it('should insert a css class with a specific name', done => {
+        let cssClass = 'foobar';
 
-            var updateFile  = 'test/ratamahatta.scss',
-                fakeContent = '.small-icon {color: #000;}',
-                updateClass = fakeContent + createdClass;
+        execute(
+          `${cli} ${fixture} --css=${createdFile} --class=${cssClass}`, (
+            err, stdout) => {
+            should.not.exist(err);
+            stdout.should.not.be.empty;
+            stdout.should.match(/created/);
+            fs.existsSync(createdFile).should.be.true;
+            fs.readFileSync(createdFile, 'utf-8').should.equal(cssExp.customName);
+            done();
+          });
+      });
 
-            beforeEach(function () {
-                fs.writeFileSync(updateFile, fakeContent);
-            });
+      it('should insert a css class with a width', done => {
+        execute(
+          `${cli} ${fixture} --css=${createdFile} --width`, (
+            err, stdout) => {
+            should.not.exist(err);
+            stdout.should.not.be.empty;
+            stdout.should.match(/created/);
+            fs.existsSync(createdFile).should.be.true;
+            fs.readFileSync(createdFile, 'utf-8').should.equal(cssExp.width);
+            done();
+          });
+      });
 
-            afterEach(function () {
-                fs.unlinkSync(updateFile);
-            });
+      it('should insert a css class with a height', done => {
+        execute(
+          `${cli} ${fixture} --css=${createdFile} --height`, (
+            err, stdout) => {
+            should.not.exist(err);
+            stdout.should.not.be.empty;
+            stdout.should.match(/created/);
+            fs.existsSync(createdFile).should.be.true;
+            fs.readFileSync(createdFile, 'utf-8').should.equal(cssExp.height);
+            done();
+          });
+      });
 
-            it('should insert a css class with the target file name', function (done) {
-                execute(cli + fixture + ' --css=' + updateFile, function (err, stdout) {
-                    should.not.exist(err);
-                    stdout.should.not.be.empty;
-                    stdout.should.match(/updated/);
-                    fs.readFileSync(updateFile, 'utf-8').should.equal(updateClass);
-                    done();
-                });
-            });
 
-            it('should insert a css class with a specific name', function (done) {
-                var cssClass    = 'pipoca',
-                    fileContent = updateClass.replace('fixture', cssClass);
+      it('should insert a css class with both width and height', done => {
+        execute(
+          `${cli} ${fixture} --css=${createdFile} --width --height`, (
+            err, stdout) => {
+            should.not.exist(err);
+            stdout.should.not.be.empty;
+            stdout.should.match(/created/);
+            fs.existsSync(createdFile).should.be.true;
+            fs.readFileSync(createdFile, 'utf-8').should.equal(cssExp.both);
+            done();
+          });
+      });
 
-                execute(cli + fixture + ' --css=' + updateFile + ' --class=' + cssClass, function (err, stdout) {
-                    should.not.exist(err);
-                    stdout.should.not.be.empty;
-                    stdout.should.match(/updated/);
-                    fs.readFileSync(updateFile, 'utf-8').should.equal(fileContent);
-                    done();
-                });
-            });
-        });
+      it('should insert a css class with both width a background-size', done => {
+        execute(
+          `${cli} ${fixture} --css=${createdFile} --background-size`, (
+            err, stdout) => {
+            should.not.exist(err);
+            stdout.should.not.be.empty;
+            stdout.should.match(/created/);
+            fs.existsSync(createdFile).should.be.true;
+            fs.readFileSync(createdFile, 'utf-8').should.equal(cssExp.bgsize);
+            done();
+          });
+      });
     });
 
+    describe('update a css file', () => {
+
+      let updateFile = 'test/ratamahatta.scss';
+      let fakeContent = '.small-icon {color: #000;}';
+      let updateClass = fakeContent + cssExp.simple;
+
+      beforeEach(() => fs.writeFileSync(updateFile, fakeContent));
+      afterEach(() => fs.unlinkSync(updateFile));
+
+      it('should insert a css class with the target file name',
+        done => {
+          execute(`${cli} ${fixture} --css=${updateFile}`, (err,
+            stdout) => {
+            should.not.exist(err);
+            stdout.should.not.be.empty;
+            stdout.should.match(/updated/);
+            fs.readFileSync(updateFile, 'utf-8').should.equal(
+              updateClass);
+            done();
+          });
+        });
+
+      it('should insert a css class with a specific name', done => {
+        let cssClass = 'pipoca',
+          fileContent = updateClass.replace('fixture', cssClass);
+
+        execute(`${cli} ${fixture} --css=${updateFile} --class=${cssClass}`, (
+            err, stdout) => {
+            should.not.exist(err);
+            stdout.should.not.be.empty;
+            stdout.should.match(/updated/);
+            fs.readFileSync(updateFile, 'utf-8').should.equal(
+              fileContent);
+            done();
+          });
+      });
+
+      it('should insert a css class with a width', done => {
+        updateClass = fakeContent + cssExp.width;
+        execute(`${cli} ${fixture} --css=${updateFile} --width`, (
+            err, stdout) => {
+            should.not.exist(err);
+            stdout.should.not.be.empty;
+            stdout.should.match(/updated/);
+            fs.existsSync(updateFile).should.be.true;
+            fs.readFileSync(updateFile, 'utf-8').should.equal(updateClass);
+            done();
+          });
+      });
+
+      it('should insert a css class with a height', done => {
+        updateClass = fakeContent + cssExp.height;
+        execute(`${cli} ${fixture} --css=${updateFile} --height`, (
+            err, stdout) => {
+            should.not.exist(err);
+            stdout.should.not.be.empty;
+            stdout.should.match(/updated/);
+            fs.existsSync(updateFile).should.be.true;
+            fs.readFileSync(updateFile, 'utf-8').should.equal(updateClass);
+            done();
+          });
+      });
+
+
+      it('should insert a css class with both width and height', done => {
+        updateClass = fakeContent + cssExp.both;
+        execute(`${cli} ${fixture} --css=${updateFile} --width --height`, (
+            err, stdout) => {
+            should.not.exist(err);
+            stdout.should.not.be.empty;
+            stdout.should.match(/updated/);
+            fs.existsSync(updateFile).should.be.true;
+            fs.readFileSync(updateFile, 'utf-8').should.equal(updateClass);
+            done();
+          });
+      });
+
+      it('should insert a css class with both width a background-size', done => {
+        updateClass = fakeContent + cssExp.bgsize;
+        execute(`${cli} ${fixture} --css=${updateFile} --background-size`, (
+            err, stdout) => {
+            should.not.exist(err);
+            stdout.should.not.be.empty;
+            stdout.should.match(/updated/);
+            fs.existsSync(updateFile).should.be.true;
+            fs.readFileSync(updateFile, 'utf-8').should.equal(updateClass);
+            done();
+          });
+      });
+    });
+  });
 });
