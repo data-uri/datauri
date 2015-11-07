643f2cd6a36c333c699b3423c88d8d25611b382f
Helder Santana <ping@heldr.com>
2015-11-07 04:10:50 -0500

    fix tests

--- test/test.js
+++ test/test.js
@@ -2,7 +2,13 @@ require('babel-core/register')({
   "presets": ["stage-0", "es2015"]
 });
 
+var path = require('path');
+
 var isProd = (process.env.NODE_ENV === 'production');
 
 global.cli_cmd = isProd ? 'node lib/datauri-cli' : './node_modules/.bin/babel-node src/cli/module.js';
 global.datauri_path = isProd ? '../../lib/datauri' : '../../src/datauri/module.js';
+
+if (isProd) {
+  process.env.DATAURI_N= process.cwd() + '/lib/datauri/index.js';
+}
